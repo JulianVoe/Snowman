b88e95763323879f40d22b929f2a85e3d7c8d549
JulianVoe <julian.voellmecke@uni-bonn.de>
2025-11-18 18:02:25 +0100

    Use correct timing method

--- reduced-timing.sh
+++ reduced-timing.sh
@@ -20,8 +20,13 @@ module load LinaroForge/25.0.3-GCCcore-13.2.0-linux-x86_64
 
 make clean && make
 
+OUT="outputs_${SLURM_JOB_ID}"; REPD="$OUT/reports"
+mkdir -p "$OUT" "$REPD"
+
+mv snowman "$OUT"
+
 # ----------------------- params -----------------------
-TASKS=(8 16 32 48 64 96)
+TASKS=(8 16 24 32 48 64 96)
 
 SS_N=1024   # strong-scaling grid
 SS_MEN=4
@@ -30,21 +35,14 @@ WS_N=128    # weak-scaling per-rank baseline (linear size = sqrt in pixels)
 WS_MEN=3
 # ------------------------------------------------------
 
-OUT="outputs_${SLURM_JOB_ID}"; REPD="$OUT/reports"
-mkdir -p "$OUT" "$REPD"
-
 SS_CSV="$OUT/strong_scaling.csv"
 WS_CSV="$OUT/weak_scaling.csv"
-echo "workers,grid,#snowmen,seconds,performance(1/s),speedup,ideal_speedup" > "$SS_CSV"
-echo "workers,grid,#snowmen,seconds,performance(1/s),efficiency,speedup" > "$WS_CSV"
+echo "workers,grid,#snowmen,seconds" > "$SS_CSV"
+echo "workers,grid,#snowmen,seconds" > "$WS_CSV"
 
-#secs() { #Times runtime and then converts "real" to seconds
-#    ( time mpirun -n "$1" ./snowman "$2" "$3" >/dev/null ) 2>&1 \
-#    | awk '/^real/ { sub(/m/, " "); sub(/s/, ""); print $2*60 + $3 }'
-#}
-avg_time() {
-  LC_ALL=C mpirun -n "$1" ./snowman "$2" "$3" \
-    | awk '/^Avg Local Computation Time/ {print $(NF-1)}'
+secs() { #Times runtime and then converts "real" to seconds
+    ( time mpirun -n "$1" "$OUT/snowman" "$2" "$3" >/dev/null ) 2>&1 \
+    | awk '/^real/ { sub(/m/, " "); sub(/s/, ""); print $2*60 + $3 }'
 }
 pixels_per_s() { awk -v N="$1" -v t="$2" 'BEGIN{printf "%.6f", (N*N)/t}'; }
 div() { awk -v a="$1" -v b="$2" 'BEGIN{printf "%.6f", a/b}'; }
@@ -52,23 +50,14 @@ mul() { awk -v a="$1" -v b="$2" 'BEGIN{printf "%.6f", a*b}'; }
 round(){ awk -v x="$1" 'BEGIN{printf "%d", (x<0)?int(x-0.5):int(x+0.5)}'; }
 
 # --- strong scaling (fixed SS_N) ---
-t0=""
 for p in "${TASKS[@]}"; do
-  t=$(avg_time "$p" "$SS_N" "$SS_MEN")
-  [[ -z "$t0" ]] && t0="$t"
-  perf=$(pixels_per_s "$SS_N" "$t")
-  echo "$p,$SS_N,$SS_MEN,$t,$perf,$(div "$t0" "$t"),$p" >> "$SS_CSV"
+  t=$(secs "$p" "$SS_N" "$SS_MEN")
+  echo "$p,$SS_N,$SS_MEN,$t" >> "$SS_CSV"
 done
 
 # --- weak scaling (N ~ WS_N * sqrt(p)) ---
-t0=""
 for p in "${TASKS[@]}"; do
   N=$(round "$(div "$(awk -v n=$WS_N -v r=$p 'BEGIN{printf "%.6f", n*sqrt(r)}')" 1)")
-  t=$(avg_time "$p" "$N" "$WS_MEN")
-  [[ -z "$t0" ]] && t0="$t"
-  perf=$(pixels_per_s "$N" "$t")
-  eff=$(div "$t0" "$t")
-  echo "$p,$N,$WS_MEN,$t,$perf,$eff,$(mul "$eff" "$p")" >> "$WS_CSV"
-  #Speedup = perf_n / perf_1  = n * t(1) / t(n)
+  t=$(secs "$p" "$N" "$WS_MEN")
+  echo "$p,$N,$WS_MEN,$t" >> "$WS_CSV"
 done
-
